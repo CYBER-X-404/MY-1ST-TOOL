df15dc4181e91f0bf9999c5521e580e6e38b6be8
CYBER-X-404 <lighterkingdom.s24@gmail.com>
2024-12-26 02:09:54 -0800

    Add files via upload

---
+++ file.py
@@ -0,0 +1,34 @@
+#FOLLOW MY GITHUB : https://github.com/MR_X
+#----------import-----------#
+import os
+#----------logo----------#
+logo=("
+[{×÷×}] ")
+#----------clear----------#
+def clear():
+  os.system('clear')
+  print(logo)
+  print(42*'-')
+  print('TG CHANNEL: GrayCyberTeam')
+  print('GITHUB : MR X')
+  print(42*'-')
+#----------line----------#
+def line():
+  print(42*'-')
+#----------menu----------#
+def main():
+   clear()
+   print( '[A] FiLE CLONNING ')
+   print(' [E] EXIT ')
+   line()
+   option=input(' [??] CHOICE MENU : ')
+   if option in ['a','A']:
+      __file__()
+    else:
+      exit(' THANKS FOR USING ')
+#----------file----------#
+def __file__()
+  clear()
+  print( 😈 YOUR PHONE HAS BEEN HACKED😈😈 )
+#----------end----------#
+main()
